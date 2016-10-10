be9884ebbafe371939d51116cfe1e728ad3fe0ff
jonna <jonakxz@gmail.com>
2016-10-10 11:28:33 +0200

    Clase File
    Métodos para cargar los datos del fichero y devolver el vector de costes y la matriz bidimensional

---
+++ practica1/File.py
@@ -0,0 +1,69 @@
+class File:
+    #Constructor de la clase
+    def __init__(self, path):
+        self.path = path
+        self.costs = []
+        self.binMatrix = []
+        self.rows = 0
+        self.columns = 0
+        self.num = 0
+        self.totalDatos = []
+
+    #Metodo para leer los datos del fichero que se le pasa en el constructor
+    def leerDatos(self):
+        infile = open(self.path, 'r')
+        for line in infile:
+            palabras = line.split()
+            for p in palabras:
+                self.totalDatos.append(p)
+                self.num += 1
+        infile.close()
+        self.rows = int(self.totalDatos[0])  # Numero de filas
+        self.columns = int(self.totalDatos[1])  # Numero de columnas
+
+        for i in range(self.rows):
+                self.binMatrix.append([0]*self.columns) #relleno la matriz con ceros
+
+        #Calculo los costes
+        contador = self.getColumns() + 2
+        for i in range(2, contador):
+            # guardo los costes
+            self.costs.append(self.totalDatos[i])
+
+        #Calculo la matriz bidimensional
+        ciudad = 0
+        datosCiudad = 0
+        for i in range(1003,self.num):
+            if datosCiudad < int(self.totalDatos[1002]):
+                self.binMatrix[int(ciudad)][int(self.totalDatos[i])-1] = 1
+                datosCiudad += 1
+            elif datosCiudad == 0:
+                ciudad += 1
+                datosCiudad = self.totalDatos[i]
+
+    #Metodo para devolver el numero de filas
+    def getRows(self):
+        return self.rows
+
+    #Metodo para devolver el numero de columnas
+    def getColumns(self):
+        return self.columns
+
+    #Muestra los datos que hay guardados en el array "Cost"
+    def mostrarCostes(self):
+        for i in range(self.costs.__len__()):
+            print  self.costs[i]
+
+    #Muestra los datos de la matriz "binMatrix"
+    def mostrarMatriz(self):
+        print self.binMatrix
+
+# fichero = File('../instancias/scp41.txt')
+# fichero.leerDatos()
+#
+# print 'Numero de ciudades: ' + fichero.rows.__str__()
+# print 'Numero de comisarias: ' + fichero.columns.__str__()
+# print 'Numero de datos que hay en el fichero: ' + fichero.num.__str__()
+# print 'El numero de datos que tiene los costes son: ' + str(fichero.costs.__len__())
+#
+# fichero.mostrarMatriz()
