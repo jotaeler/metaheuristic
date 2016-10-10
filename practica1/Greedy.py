ab370c074ac74c8fedbb9f9fddbb598b697cf34c
Jose Antonio López Rufián <jalr0005@red.ujaen.es>
2016-10-10 11:56:26 +0200

    temp Commit

--- practica1/Greedy.py
+++ practica1/Greedy.py
@@ -11,7 +11,9 @@ class Greedy:
 		self.solution = list()			# List with final solution
 		self.covered = list()	# Meaning each position is covered by X subsec
 		self.ratios = self.getRatios()		# Dict with subsector (key) ratios (value)
-		self.subsCovers=self.calcSubsectorsCover() #covers ordered
+		self.totalCosts= self.calcTotalCosts() # List with total cost of each subsector
+		self.subsCoversList=self.calcSubsectorsCover() #covers
+		self.subsCoversOrdered=self.calcSubsectorsCoverDict() #covers ordered
 	"""
 	Returns how many sectors (cities) are covered by one
 	subsector a.k.a. hospital
@@ -26,33 +28,50 @@ class Greedy:
 	Return an ordered dictionary where key is subsector and value number of
 	sectors covered by it
 	"""
-	def calcSubsectorsCover(self):
+	def calcSubsectorsCoverDict(self):
 		ret={}
+		for x in range(0,self.subsectors-1):	# -1 because list start at 0
+			ret[x]=self.subsCoversList[x]
+		return sorted(ret.items(), key=lambda t: t[1])		# ordered
+		
+	"""
+	Return a list with number of sectors covered by subsector (list position)
+	"""
+	def calcSubsectorsCover(self):
+		ret=[]
 		for x in range(0,self.subsectors-1):	# -1 because list start at 0
 			total=0
 			for y in range(0,self.sectors-1):	# -1 because list start at 0
 				total=total+self.matrix[y][x]	# moving through sectors first
 			ret[x]=total
-		return sorted(ret.items(), key=lambda t: t[1])		# ordered
-	
+		return ret
+		
 	"""
-	Returns subsector total cost
+	Return list with total costs
 	"""
-	def getSubsectorCost(self, subsector):
-		cost=0
+	def calcTotalCosts(self):
+		ret=[]
 		for x in range(0, self.subsectors-1):
-			cost=cost+(self.costs[subsector]*self.matrix[x][subsector])
-		return cost
-	
+			ret[x]=self.subsCoversList*self.costs[x]
+		return ret
+		
 	"""
 	Returns subsector ratio
 	"""
 	def getRatios(self):
 		ret={}
 		for x in range(len(self.costs)):
-			ret[x](self.subsCovers[x]/self.getSubsectorCost(x))
+			ret[x]=(self.subsCoversList[x]/self.totalCosts[x])
 		return sorted(ret.items(), key=lambda t: t[1])		# ordered
-
+	
+	"""
+	Auxiliar function for Delete, return ordered dict with covers only for
+	selected subsectors
+	"""
+	def getSelectedCovers(self):
+		for x in range(0, self.subsectors):
+			if(self.solution[x]==1):
+				
 	"""
 	Delete unnecessary subsectors a.k.a. hospitals
 	"""
@@ -63,7 +82,7 @@ class Greedy:
 	Returns the next subsector to be taken
 	"""
 	def select(self):
-		pass
+		for x in range(0, self.subsectors-1):
 	
 	"""
 	Returns the solution
