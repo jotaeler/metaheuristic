d48444ba4588f97d5f38ca21a0a5f432f9cc1579
jotaeler <lopezru.joseantonio@gmail.com>
2016-10-25 19:42:02 +0200

    TODO check functionallity

--- practica1/Grasp.py
+++ practica1/Grasp.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from collections import OrderedDict
+from LocalSearch import LocalSearch
 import random
 import copy
 
@@ -19,16 +20,32 @@ class Grasp:
         self.matrix = matrix  # Bin matrix meaning wich subsctr covers each sec
         self.matrixDict={}
         for x in range(self.sectors):
-            self.matrixDict[x]=self.matrix[x]
+            self.matrixDict[x] = self.matrix[x]
+        self.neihMatrixDict = copy.deepcopy(self.matrixDict)
         self.solution = list()            # List with final solution
         for x in range(subsectors):
             self.solution.append(0)
+        self.bestSolution = [x*0 for x in range(self.subsectors)]
+        self.solCost = 0
+        self.bestSolutionCost = 0
         self.covered = list()    # Meaning each position is covered by X subsec
         for x in range(sectors):
             self.covered.append(0)
+        self.subsNoCoversList = self.calcSubsectorsNoCover()  # covers
         self.subsCoversList = self.calcSubsectorsCover()  # covers
         self.subsCoversOrdered = self.calcSubsectorsCoverDict()  # covers ordered
-        self.solCost=0
+        self.limit = limit
+
+
+    """
+    Return an ordered dictionary where key is subsector and value number of
+    sectors covered by it
+    """
+    def calcSubsectorsCoverDict(self):
+        ret = {}
+        for x in range(0, self.subsectors):  # -1 because list start at 0
+            ret[x] = self.subsCoversList[x]
+        return sorted(ret.items(), key=lambda t: t[1])  # ordered
 
     """
     Return a list with number of sectors covered by subsectors (list position)
@@ -38,8 +55,34 @@ class Grasp:
         for x in range(self.subsectors):
             total = 0
             for y in self.matrixDict.keys():
-                total = total+self.matrixDict[y][x]    # moving through sectors first
+                total = total + self.matrixDict[y][x]  # moving through sectors first
             ret.append(total)
+        return ret
+    """
+    Return a list with number of sectors covered by subsectors (list position)
+    """
+    def calcSubsectorsNoCover(self):
+        ret = []
+        for x in range(self.subsectors):
+            total = 0
+            for y in self.neihMatrixDict.keys():
+                if self.neihMatrixDict[y][x] == 0:
+                    total += 1
+            ret.append(total)
+        return ret
+
+    """
+    Return a list with number of sectors covered by subsectors (list position)
+    """
+    def calcNObjNoCov(self):
+        ret = 0
+        for x in range(self.subsectors):
+            total = 0
+            for y in self.neihMatrixDict.keys():
+                if self.neihMatrixDict[y][x] == 0:
+                    total += 1
+            if total > ret:
+                ret = total
         return ret
 
     """
@@ -96,25 +139,68 @@ class Grasp:
     def recalculateMatrix(self, subsector):
         self.setSectorsAtCovered(subsector)
         self.subsCoversList[subsector] = 0
-        delete=[]
-        for x in self.matrixDict.keys():
-            if(self.matrixDict[x][subsector] == 1):
+        delete = []
+        for x in self.neihMatrixDict.keys():
+            if(self.neihMatrixDict[x][subsector] == 1):
                 delete.append(x)
         for x in delete:
-            self.matrixDict.pop(x,None)
-        self.subsCoversList = self.calcSubsectorsCover()
-
+            self.neihMatrixDict.pop(x,None)
 
     """
+    Calc Solution Cost
+    """
+    def solutionCost(self,solution):
+        total = 0
+        for x in range(self.subsectors):
+            if (solution[x] == 1):
+                total += self.costs[x]
+        return total
 
+    """
+    Randomize Greedy through RCL list
     """
     def randomizedGreedy(self):
-        auxMatrixDict = copy.deepcopy(self.matrixDict)
-        while len(auxMatrixDict) != 0:
+        while len(self.neihMatrixDict) != 0:
             rcl=[]
-            NObjNoCov = len(auxMatrixDict)*0.7
-            for x in range(self.subsCoversList):
-                if(self.subsCoversList[x] >= NObjNoCov):
+            NObjNoCov = self.calcNObjNoCov()*0.7
+            for x in range(self.subsectors):
+                if self.subsNoCoversList[x] >= NObjNoCov and self.subsCoversList[x] != 0:
                     rcl.append(x)
-            # Make RCL list
-            # select random
+            rand = random.randint(0, len(rcl)-1)
+            candidate = rcl[rand]
+            self.solution[candidate] = 1
+            self.recalculateMatrix(candidate)
+        self.delete()
+        return self.solution
+
+    """
+    Copy data structures
+    """
+    def updateMemory(self):
+        self.neihMatrixDict = copy.deepcopy(self.matrixDict)
+        self.subsNoCoversList = self.calcSubsectorsNoCover()  # covers
+        self.subsCoversList = self.calcSubsectorsCover()  # covers
+        for x in range(self.subsectors):
+            self.solution[x] = 0
+        for x in range(self.sectors):
+            self.covered[x] = 0
+
+    """
+    Start Grasp Algorithm
+    """
+    def start(self):
+        iterations = 0
+        while iterations < self.limit:
+            self.solution = self.randomizedGreedy()
+            cost = self.solutionCost(self.solution)
+            print("Coste de la solucion random="+str(cost))
+            bl = LocalSearch(self.costs, self.matrix, self.sectors, self.subsectors, self.solution, cost, self.covered, "1234", 400)
+            bl.start()
+            if bl.bestSolutionCost < self.bestSolutionCost:
+                print("Mejor solucion encontrada con coste ="+str(bl.bestSolutionCost))
+                self.bestSolution = bl.bestSolution
+                self.bestSolutionCost = bl.bestSolutionCost
+            iterations += (bl.iterations + 1)
+            self.updateMemory()
+            print("Iterations ="+str(iterations))
+        print("Finalizado GRASP con coste="+str(self.bestSolutionCost))
--- practica1/main.py
+++ practica1/main.py
@@ -1,24 +1,23 @@
 from File import File
 from Greedy import Greedy
 from LocalSearch import LocalSearch
+from Grasp import Grasp
+
+import numpy as np
 
 fichero = File('../instancias/scp41.txt')
 fichero.leerDatos()
 
-#print ('Matriz ' + str(fichero.binMatrix))
-print('Numero de datos que hay en el fichero: ' + fichero.num.__str__())
-print('Numero de comisarias: ' + fichero.columns.__str__())
-print('Numero de ciudades: ' + fichero.rows.__str__())
-print('El numero de datos que tiene los costes son: ' + str(fichero.costs.__len__()))
-
 greedy = Greedy(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns,"1234")
 greedy.start()
-#print("Covered= "+str(greedy.covered))
-#print("Solution= "+str(greedy.solution))
-print("Solution cost="+str(greedy.solCost))
 
-bl=LocalSearch(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns, greedy.solution, greedy.solCost, greedy.covered, "1234",10000)
-bl.start()
-#print("Covered= "+str(bl.covered))
-#print("Solution= "+str(bl.bestSolution))
-print("Solution cost="+str(bl.bestSolutionCost))
+print("Solution cost GREEDY="+str(greedy.solCost))
+
+# bl=LocalSearch(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns, greedy.solution, greedy.solCost, greedy.covered, "1234",10000)
+# bl.start()
+# print("Busqueda LOCAL Solution cost="+str(bl.bestSolutionCost))
+
+grasp = Grasp(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns,"1234", 10000)
+grasp.start()
+
+print("Solution cost GREEDY="+str(greedy.solCost))
