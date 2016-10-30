0923508bdd0e40c64dafd58e99662f7512ceb099
jotaeler <lopezru.joseantonio@gmail.com>
2016-10-30 13:47:23 +0100

    ejecuciones de algoritmos

--- practica1/main.py
+++ practica1/main.py
@@ -2,22 +2,125 @@ from File import File
 from Greedy import Greedy
 from LocalSearch import LocalSearch
 from Grasp import Grasp
+import time
 
-import numpy as np
 
-fichero = File('../instancias/scp41.txt')
+fichero = File('../instancias/scpnrf1.txt')
 fichero.leerDatos()
 
-greedy = Greedy(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns,"1234")
-greedy.start()
+# t0 = time.clock()
+# greedy = Greedy(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns,"77373014")
+# greedy.start()
+# end = time.clock() -t0
+# print("Ejecucion 1 = "+str(end))
+# print("Solution cost GREEDY="+str(greedy.solCost))
+# print("-------------------------------------------")
 
-print("Solution cost GREEDY="+str(greedy.solCost))
-
-# bl=LocalSearch(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns, greedy.solution, greedy.solCost, greedy.covered, "1234",10000)
+# t0 = time.clock()
+# greedy = Greedy(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns,"47737301")
+# greedy.start()
+# end = time.clock() -t0
+# print("Ejecucion 2 = "+str(end))
+# print("Solution cost GREEDY="+str(greedy.solCost))
+# print("-------------------------------------------")
+#
+# t0 = time.clock()
+# greedy = Greedy(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns,"14773730")
+# greedy.start()
+# end = time.clock() -t0
+# print("Ejecucion 3 = "+str(end))
+# print("Solution cost GREEDY="+str(greedy.solCost))
+# print("-------------------------------------------")
+#
+# t0 = time.clock()
+# greedy = Greedy(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns,"01477373")
+# greedy.start()
+# end = time.clock() -t0
+# print("Ejecucion 4 = "+str(end))
+# print("Solution cost GREEDY="+str(greedy.solCost))
+# print("-------------------------------------------")
+#
+# t0 = time.clock()
+# greedy = Greedy(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns,"30147737")
+# greedy.start()
+# end = time.clock() -t0
+# print("Ejecucion 5 = "+str(end))
+# print("Solution cost GREEDY="+str(greedy.solCost))
+# print("-------------------------------------------")
+# t0 = time.clock()
+# bl=LocalSearch(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns, greedy.solution, greedy.solCost, greedy.covered, "77373014",10000)
 # bl.start()
+# end = time.clock() -t0
+# print("Ejecucion 1 = "+str(end))
 # print("Busqueda LOCAL Solution cost="+str(bl.bestSolutionCost))
-
-grasp = Grasp(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns,"1234", 10000)
+# print("-------------------------------------------")
+#
+# t0 = time.clock()
+# bl=LocalSearch(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns, greedy.solution, greedy.solCost, greedy.covered, "47737301",10000)
+# bl.start()
+# end = time.clock() -t0
+# print("Ejecucion 2 = "+str(end))
+# print("Busqueda LOCAL Solution cost="+str(bl.bestSolutionCost))
+# print("-------------------------------------------")
+#
+# t0 = time.clock()
+# bl=LocalSearch(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns, greedy.solution, greedy.solCost, greedy.covered, "14773730",10000)
+# bl.start()
+# end = time.clock() -t0
+# print("Ejecucion 3 = "+str(end))
+# print("Busqueda LOCAL Solution cost="+str(bl.bestSolutionCost))
+# print("-------------------------------------------")
+#
+# t0 = time.clock()
+# bl=LocalSearch(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns, greedy.solution, greedy.solCost, greedy.covered, "01477373",10000)
+# bl.start()
+# end = time.clock() -t0
+# print("Ejecucion 4 = "+str(end))
+# print("Busqueda LOCAL Solution cost="+str(bl.bestSolutionCost))
+# print("-------------------------------------------")
+#
+# t0 = time.clock()
+# bl=LocalSearch(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns, greedy.solution, greedy.solCost, greedy.covered, "30147737",10000)
+# bl.start()
+# end = time.clock() -t0
+# print("Ejecucion 5 = "+str(end))
+# print("Busqueda LOCAL Solution cost="+str(bl.bestSolutionCost))
+# print("-------------------------------------------")
+t0 = time.clock()
+grasp = Grasp(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns,"77373014", 10000)
 grasp.start()
+end = time.clock() -t0
+print("Ejecucion 1 = "+str(end))
+print("Solution cost GRASP="+str(grasp.bestSolutionCost))
+print("-------------------------------------------")
 
-print("Solution cost GRASAP="+str(grasp.bestSolutionCost))
+t0 = time.clock()
+grasp = Grasp(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns,"47737301", 10000)
+grasp.start()
+end = time.clock() -t0
+print("Ejecucion 2 = "+str(end))
+print("Solution cost GRASP="+str(grasp.bestSolutionCost))
+print("-------------------------------------------")
+
+t0 = time.clock()
+grasp = Grasp(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns,"14773730", 10000)
+grasp.start()
+end = time.clock() -t0
+print("Ejecucion 3 = "+str(end))
+print("Solution cost GRASP="+str(grasp.bestSolutionCost))
+print("-------------------------------------------")
+
+t0 = time.clock()
+grasp = Grasp(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns,"01477373", 10000)
+grasp.start()
+end = time.clock() -t0
+print("Ejecucion 4 = "+str(end))
+print("Solution cost GRASP="+str(grasp.bestSolutionCost))
+print("-------------------------------------------")
+
+t0 = time.clock()
+grasp = Grasp(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns,"30147737", 10000)
+grasp.start()
+end = time.clock() -t0
+print("Ejecucion 5 = "+str(end))
+print("Solution cost GRASP="+str(grasp.bestSolutionCost))
