a9ff1a216b26f5bce573c54f76b5516bb07db65b
jotaeler <lopezru.joseantonio@gmail.com>
2016-11-25 16:00:01 +0100

    Best individual

--- practica2/main.py
+++ practica2/main.py
@@ -2,15 +2,19 @@ from generation import Generation
 from File import File
 import time
 
-fichero = File('../instancias/scp41.txt')
+fichero = File('../instancias/scpa1.txt')
 fichero.leerDatos()
 
 generacional = Generation(fichero.costs, fichero.binMatrix, fichero.rows, fichero.columns)
 t0 = time.clock()
 poblacion_evolucionada = generacional.runGenerationEvolverHUX()
+best = poblacion_evolucionada[0]
+for organism in poblacion_evolucionada:
+    if organism.fitness < best.fitness:
+        best = organism
 end = time.clock() -t0
 print("Ejecucion  = "+str(end))
-print(str(poblacion_evolucionada[0].fitness))
+print(str(best.fitness))
 
 
 
